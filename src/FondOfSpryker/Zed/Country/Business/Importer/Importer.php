699b3a329dee175caecabb3a7fd2067aaa22b272
Julian Zimmermann <julian.zimmermann@fondof.de>
2020-02-05 14:03:08 +0100

    spryker update

--- src/FondOfSpryker/Zed/Country/Business/Importer/Importer.php
+++ src/FondOfSpryker/Zed/Country/Business/Importer/Importer.php
@@ -24,8 +24,8 @@ class Importer implements ImporterInterface
     protected $regionManager;
 
     /**
-     * @param \Spryker\Zed\Country\Business\CountryManagerInterface $countryManager
-     * @param \Spryker\Zed\Country\Business\RegionManagerInterface $regionManager
+     * @param \FondOfSpryker\Zed\Country\Business\CountryManagerInterface $countryManager
+     * @param \FondOfSpryker\Zed\Country\Business\RegionManagerInterface $regionManager
      * @param \FondOfSpryker\Zed\Country\CountryConfig $config
      */
     public function __construct(
